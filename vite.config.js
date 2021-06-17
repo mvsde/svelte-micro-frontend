c2919205dad4e4a9a02660a71164cbc8d807f891
Fynn Becker <post@fynn.be>
2021-06-17 07:20:46 +0200

    feat: Use explicit Vite IIFE configuration

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,19 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
   plugins: [
-    svelte()
+    svelte({
+      emitCss: false
+    })
   ],
 
   build: {
     assetsDir: '',
     sourcemap: true,
-    rollupOptions: {
-      input: 'src/main.js',
-      output: {
-        format: 'iife',
-        entryFileNames: 'app.js'
-      }
+    lib: {
+      entry: 'src/main.js',
+      formats: ['iife'],
+      name: 'SvelteMicroFrontend',
+      fileName: 'svelte-micro-frontend'
     }
   }
 })
